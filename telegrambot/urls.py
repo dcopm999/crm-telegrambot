ab953857bafe3192a520f4c8c957cdb1b27b52e8
Pavel Tanchev <dcopm999@gmail.com>
2020-09-15 16:35:24 +0500

    telebot
    * requirements.txt: added requirements for channels and pyTelegramBotAPI
    * telegrambot.bot: added class TelegramBot
    * telegrambot.consumers: added class TelegramConsumer
    * telegrambot.routing: added route for TelegramConsumer

--- telegrambot/urls.py
+++ telegrambot/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import TemplateView
 
 from . import views
@@ -7,5 +7,5 @@ from . import views
 
 app_name = 'telegrambot'
 urlpatterns = [
-    url(r'', TemplateView.as_view(template_name="base.html")),
-    ]
+    path(r'', TemplateView.as_view(template_name="base.html")),
+]
