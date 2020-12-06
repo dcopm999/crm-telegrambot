a8a87a1eb166b3c6f50af6d4cd301e064e45d798
dcopm999 <dcopm999@gmail.com>
2020-12-06 16:52:38 +0500

    Aiogram (#9)
    * tests.settings
    
    * added typing for ALLOWED_HOSTS
    
    * .travis.yml
    
    * added env for django 3.1
    
    * requirements.txt
    
    * added requirement for django-environ
    * added requirement for django-allauth
    * added requirement for aiogram
    
    * tox.ini
    
    * added env for django 3.1
    * added makemigrations and migrate commands befor runtest
    
    * requirements_dev.txt
    
    * added requirement for pre-commit
    * added requirement django-stubs
    
    * setup.cfg
    
    * added flake8 settings
    * added mypy settings
    * added coverage settings
    
    * tests/settings.py
    
    * added django.contrib.sites to INSTALLED_APPS
    * added allauth to INSTALLED_APPS
    * added products to INSTALLED_APPS
    
    * .pre-commit-config.yaml
    
    * added pre-commit settings
    
    * .mypy_cache/
    
    * added excludes for mypy
    * added excludes for sqlite db
    
    * tests/settings.py
    
    * removed products add from INSTALLD_APPS
    
    * TelegramBot rev0.1
    
    * requirements_dev.txt: added requirement for ipython
    * telegrambot.bot.TelegramBot: getting settings from SocialApp
    
    Co-authored-by: Павел Таньчев <p.tanchev@spider.ru>

--- telegrambot/bot.py
+++ telegrambot/bot.py
@@ -1,70 +1,91 @@
 import logging
-from telebot import TeleBot, types, apihelper
 
-from django.conf import settings
+from allauth.socialaccount.models import SocialApp
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-
-from products import models as product_models
+from products import models
+from telebot import TeleBot, apihelper, types
 
 logger = logging.getLogger(__name__)
 
 
 class TelegramBot:
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(TelegramBot, cls).__new__(cls)
-        return cls.instance
-
     def __init__(self):
-        self.TOKEN = settings.TELEGRAM_TOKEN
-        self.bot = TeleBot(self.TOKEN)
-        self.WEBHOOK_URL = f'https://{settings.TELEGRAM_WEBHOOK_HOST}{settings.TELEGRAM_WEBHOOK_PATH}'
-        logger.debug('%s: __init__()', self.__class__)
+        social_app = SocialApp.objects.last()
+        TELEGRAM_TOKEN = social_app.secret
+        TELEGRAM_WEBHOOK_HOST = social_app.sites.last().domain
+        TELEGRAM_WEBHOOK_PATH = reverse("telegrambot:webhook")
+        self.bot = TeleBot(TELEGRAM_TOKEN)
+        self.WEBHOOK_URL = f"https://{TELEGRAM_WEBHOOK_HOST}{TELEGRAM_WEBHOOK_PATH}"
+        logger.debug("%s: __init__()", self.__class__)
 
-    def send_message(self, chat_id:int, text:str, reply_markup=None):
+    def send_message(self, chat_id: int, text: str, reply_markup=None):
         try:
             self.bot.send_message(chat_id, text, reply_markup=reply_markup)
         except apihelper.ApiTelegramException as msg:
             logger.exception(msg)
-        logger.debug(f'{self.__class__}: send_message({chat_id}, {text})')
+        logger.debug(f"{self.__class__}: send_message({chat_id}, {text})")
 
-    def send_photo(self, chat_id: str, photo_path: str, caption=None, reply_markup=None):
-        with open(photo_path, 'rb') as photo_file:
+    def send_photo(
+        self, chat_id: str, photo_path: str, caption=None, reply_markup=None
+    ):
+        with open(photo_path, "rb") as photo_file:
             photo = photo_file.read()
         try:
-            self.bot.send_photo(chat_id, photo=photo, caption=caption, reply_markup=reply_markup)
+            self.bot.send_photo(
+                chat_id, photo=photo, caption=caption, reply_markup=reply_markup
+            )
         except apihelper.ApiTelegramException as msg:
             logger.exception(msg)
-            logger.info(f'{self.__class__}: User {chat_id} baned your TelegramBot')
+            logger.info(f"{self.__class__}: User {chat_id} baned your TelegramBot")
         else:
-            logger.debug(f'{self.__class__}: send_photo({chat_id}, {photo_path})')
+            logger.debug(f"{self.__class__}: send_photo({chat_id}, {photo_path})")
+
+    def answer_callback_query(
+        self, callback_id, text=None, show_alert=None, url=None, cache_time=None
+    ):
+        self.bot.answer_callback_query(
+            callback_id,
+            text=text,
+            show_alert=show_alert,
+            url=url,
+            cache_time=cache_time,
+        )
 
     def set_webhook(self):
-        logger.debug('%s: set_hook() %s' % (self.__class__, self.WEBHOOK_URL))
+        logger.debug("%s: set_hook() %s" % (self.__class__, self.WEBHOOK_URL))
         return self.bot.set_webhook(url=self.WEBHOOK_URL)
 
     def remove_webhook(self):
-        logger.debug('%s: remove_webhook()', self.__class__)
+        logger.debug("%s: remove_webhook()", self.__class__)
         return self.bot.remove_webhook()
 
-    def get_kb_phone():
+    def get_kb_phone(self):
         keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-        button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
-        button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
+        button_phone = types.KeyboardButton(
+            text="Отправить номер телефона", request_contact=True
+        )
+        button_geo = types.KeyboardButton(
+            text="Отправить местоположение", request_location=True
+        )
         keyboard.add(button_phone, button_geo)
         return keyboard
 
     def get_kb_catalog(self):
-        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
-        buttons = [types.KeyboardButton(text=item.name) for item in product_models.Catalog.objects.all()]
+        keyboard = types.ReplyKeyboardMarkup(
+            row_width=1, resize_keyboard=True, one_time_keyboard=True
+        )
+        buttons = [
+            types.KeyboardButton(text=item.name)
+            for item in models.Catalog.objects.all()
+        ]
         keyboard.add(*buttons)
         return keyboard
 
     def get_kb_inline_buy(self, product):
         keyboard = types.InlineKeyboardMarkup()
-        buy = _('Buy')
-        text = f'{buy}: {product.name}'
+        buy = _("Buy")
+        text = f"{buy}: {product.name}"
         buy_btn = types.InlineKeyboardButton(text, callback_data=str(product.id))
-
         keyboard.add(buy_btn)
         return keyboard
