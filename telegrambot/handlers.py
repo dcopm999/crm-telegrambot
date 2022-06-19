4463cb7a0c2fe86e38485d18d59119d4cd5472e9
dcopm999 <dcopm999@gmail.com>
2022-06-19 23:27:45 +0500

    Dev (#10)
    * .pre-commit-config.yaml: updated pre-comit settings
    
    * Added Sunscriber model
    
    * telegrambot/models.py: Added Subscriber model
    * telegrambot/admin.py: Added Subscriber model to admin panel
    * telegrambot/handlers.py: Linting
    * telegrambot/mixins.py: Updated add_username method in HistoryMixin
    
    * Update requirements
    
    * requirements.txt: Updated

--- telegrambot/handlers.py
+++ telegrambot/handlers.py
@@ -1,8 +1,9 @@
 import logging
 from abc import ABC, abstractmethod
 from typing import Any, Optional
+
 from telegrambot.bot import TelegramBot
-from telegrambot.mixins import HistoryMixin, CatalogMixin
+from telegrambot.mixins import CatalogMixin, HistoryMixin
 
 logger = logging.getLogger(__name__)
 
@@ -27,16 +28,17 @@ class AbstractHandler(Handler):
     Поведение цепочки по умолчанию может быть реализовано внутри базового класса
     обработчика.
     """
+
     _next_handler: Handler = None
 
     def __init__(self):
-        logger.debug('%s: __init__()', self.__class__)
+        logger.debug("%s: __init__()", self.__class__)
         self.bot = TelegramBot()
 
     def set_next(self, handler: Handler) -> Handler:
-        logger.debug('%s: set_next()', self.__class__)
+        logger.debug("%s: set_next()", self.__class__)
         self._next_handler = handler
-        logger.debug('%s: self._next_handler=%s' % (self.__class__, handler))
+        logger.debug("%s: self._next_handler=%s" % (self.__class__, handler))
         # Возврат обработчика отсюда позволит связать обработчики простым
         # способом, вот так:
         # monkey.set_next(squirrel).set_next(dog)
@@ -53,8 +55,11 @@ class HandlerCommandBase(AbstractHandler):
     command: str
 
     def handle(self, request: Any) -> str:
-        logger.debug('%s: handle()', self.__class__)
-        if request.get('callback_query', False) or request.get('message').get('text') == self.command or self.command == '__all__':
+        logger.debug("%s: handle()", self.__class__)
+        if (
+            request.get("callback_query", False)
+            or request.get("message").get("text") == self.command  # noqa: W503
+        ):
             self.run(request)
         super().handle(request)
 
@@ -66,48 +71,50 @@ class HandlerCommandBase(AbstractHandler):
 
 
 class HandlerCommandStart(HistoryMixin, HandlerCommandBase):
-    command = '/start'
+    command = "/start"
 
     def run(self, request):
-        logger.debug('%s: run()', self.__class__)
-        if request.get('message'):
-            chat_id = request.get('message').get('chat').get('id')
+        logger.debug("%s: run()", self.__class__)
+        if request.get("message"):
+            chat_id = request.get("message").get("chat").get("id")
             keyboard = self.bot.get_kb_catalog()
-            self.bot.send_message(chat_id, 'Catalog', reply_markup=keyboard)
+            self.bot.send_message(chat_id, "Catalog", reply_markup=keyboard)
         super(HandlerCommandStart, self).run(request)
 
     def get_result(self):
-        return 'started'
+        return "started"
 
 
 class HandlerCommandHelp(HandlerCommandBase):
-    command = '/help'
+    command = "/help"
 
     def get_result(self):
-        return 'helped'
+        return "helped"
 
 
 class HandlerProductList(CatalogMixin, HandlerCommandBase):
-    command = '__all__'
+    command = "__all__"
 
     def run(self, request):
-        if request.get('message'):
-            chat_id = request.get('message').get('chat').get('id')
-            category = request.get('message').get('text')
+        if request.get("message"):
+            chat_id = request.get("message").get("chat").get("id")
+            category = request.get("message").get("text")
             products = self.get_product_by_categoty(category)
             if products is None:
                 pass
             else:
                 for product in products:
                     buy_btn = self.bot.get_kb_inline_buy(product)
-                    self.bot.send_photo(chat_id, product.image.path, product.caption, buy_btn)
+                    self.bot.send_photo(
+                        chat_id, product.image.path, product.caption, buy_btn
+                    )
 
 
 class HandlerProductOrder(CatalogMixin, HandlerCommandBase):
-    command = '__all__'
+    command = "__all__"
 
     def run(self, request):
-        if request.get('callback_query'):
-            chat_id = request.get('callback_query').get('message').get('chat').get('id')
-            product_id = request.get('callback_query').get('data')
+        if request.get("callback_query"):
+            chat_id = request.get("callback_query").get("message").get("chat").get("id")
+            product_id = request.get("callback_query").get("data")
             self.set_product_order(chat_id=chat_id, product_id=product_id)
--- telegrambot/mixins.py
+++ telegrambot/mixins.py
@@ -1,31 +1,35 @@
 import logging
+
 from django.contrib.auth import get_user_model
+from products import models as products_models
 
 from telegrambot import models as telegrambot_models
-from products import models as products_models
 
 logger = logging.getLogger(__name__)
 
 
 class HistoryMixin:
     def __init__(self):
-        self.subscriber_model = get_user_model()
+        self.user_model = get_user_model()
+        self.subscriber_model = telegrambot_models.Subscriber
         self.request_model = telegrambot_models.Request
         self.response_model = telegrambot_models.Response
         self.dialog_model = telegrambot_models.Dialog
         super(HistoryMixin, self).__init__()
 
     def run(self, request):
-        logger.debug('%s: run()', self.__class__)
-        if request.get('message', False):
-            logger.debug(f'{self.__class__}: is_message')
-            self.chat_id = request.get('message').get('from').get('id')
-            self.message_from = request.get('message').get('from')
-            text_request = request.get('message').get('text')
-        elif request.get('callback_query', False):
-            self.chat_id = request.get('callback_query').get('message').get('chat').get('id')
-            self.message_from = request.get('callback_query').get('from')
-            text_request = request.get('callback_query').get('message').get('caption')
+        logger.debug("%s: run()", self.__class__)
+        if request.get("message", False):
+            logger.debug(f"{self.__class__}: is_message")
+            self.chat_id = request.get("message").get("from").get("id")
+            self.message_from = request.get("message").get("from")
+            text_request = request.get("message").get("text")
+        elif request.get("callback_query", False):
+            self.chat_id = (
+                request.get("callback_query").get("message").get("chat").get("id")
+            )
+            self.message_from = request.get("callback_query").get("from")
+            text_request = request.get("callback_query").get("message").get("caption")
         text_response = self.get_result()
         subscriber_query = self.subscriber_add(request)
         request_query = self.request_add(subscriber_query, text_request)
@@ -33,36 +37,62 @@ class HistoryMixin:
         self.dialog_add(subscriber_query, request_query, response_query)
 
     def subscriber_add(self, request):
-        logger.debug(f'{self.__class__}: subscriber_add()')
-        username = self.message_from.get('username', f'telegram_{self.chat_id}')
-        self.message_from['username'] = username
-        if 'id' in self.message_from.keys():
-            self.message_from.pop('id')
+        logger.debug(f"{self.__class__}: subscriber_add()")
+        username = self.message_from.get("username", f"telegram_{self.chat_id}")
+        self.message_from["username"] = username
+        if "id" in self.message_from.keys():
+            self.message_from.pop("id")
 
         try:
             subscriber = self.subscriber_model.objects.get(telegram_id=self.chat_id)
         except self.subscriber_model.DoesNotExist:
-            logger.debug(f'User create: telegram_id={self.chat_id} message_from={self.message_from}')
-            subscriber = self.subscriber_model(telegram_id=self.chat_id, **self.message_from)
-            subscriber.save()
-            logger.debug(f'{self.__class__}: Created new subscriber {subscriber}')
-        else:
-            logger.debug(f'User update: telegram_id={self.chat_id} message_from={self.message_from}')
-            self.subscriber_model.objects.select_for_update().filter(telegram_id=self.chat_id).update(
-                telegram_id=self.chat_id,
-                **self.message_from
+            logger.debug(
+                f"User create: telegram_id={self.chat_id} message_from={self.message_from}"
             )
-            logger.debug(f'{self.__class__}: Updated subscriber {subscriber}')
+            user, _ = self.user_model.objects.get_or_create(
+                username=self.message_from["username"]
+            )
+            subscriber = self.subscriber_model(
+                user=user,
+                telegram_id=self.chat_id,
+                name=self.message_from["first_name"],
+                is_bot=self.message_from["is_bot"],
+            )
+            # subscriber = self.subscriber_model(telegram_id=self.chat_id, **self.message_from)
+            subscriber.save()
+            logger.debug(f"{self.__class__}: Created new subscriber {subscriber}")
+        else:
+            logger.debug(
+                f"User update: telegram_id={self.chat_id} message_from={self.message_from}"
+            )
+            user, _ = self.user_model.objects.get_or_create(
+                username=self.message_from["username"]
+            )
+            self.subscriber_model.objects.select_for_update().filter(
+                telegram_id=self.chat_id
+            ).update(
+                user=user,
+                telegram_id=self.chat_id,
+                name=self.message_from["first_name"],
+                is_bot=self.message_from["is_bot"],
+            )
+            logger.debug(f"{self.__class__}: Updated subscriber {subscriber}")
         return subscriber
 
     def request_add(self, subscriber_query, text_request):
-        return self.request_model.objects.create(subscriber=subscriber_query, text=text_request)
+        return self.request_model.objects.create(
+            subscriber=subscriber_query, text=text_request
+        )
 
     def response_add(self, subscriber_query, text_response):
-        return self.response_model.objects.create(subscriber=subscriber_query, text=text_response)
+        return self.response_model.objects.create(
+            subscriber=subscriber_query, text=text_response
+        )
 
     def dialog_add(self, subscriber_query, request_query, response_query):
-        return self.dialog_model.objects.create(subscriber=subscriber_query, request=request_query, response=response_query)
+        return self.dialog_model.objects.create(
+            subscriber=subscriber_query, request=request_query, response=response_query
+        )
 
 
 class CatalogMixin:
