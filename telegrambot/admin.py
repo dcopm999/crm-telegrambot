04c0dbb9daeb7007ae9a8c13ed17ab62f2d4400a
master <dcopm999@gmail.com>
2022-06-19 23:05:00 +0500

    Added Sunscriber model
    * telegrambot/models.py: Added Subscriber model
    * telegrambot/admin.py: Added Subscriber model to admin panel
    * telegrambot/handlers.py: Linting
    * telegrambot/mixins.py: Updated add_username method in HistoryMixin

--- telegrambot/admin.py
+++ telegrambot/admin.py
@@ -4,8 +4,14 @@ from django.contrib import admin
 from telegrambot import models
 
 
+@admin.register(models.Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ["telegram_id", "user"]
+    date_hierarchy = "created"
+
+
 @admin.register(models.Dialog)
 class DialogAdmin(admin.ModelAdmin):
-    list_display = ['subscriber', 'request', 'response', 'created']
-    date_hierarchy = 'created'
-    readonly_fields = ['subscriber', 'request', 'response']
+    list_display = ["subscriber", "request", "response", "created"]
+    date_hierarchy = "created"
+    readonly_fields = ["subscriber", "request", "response"]
--- telegrambot/models.py
+++ telegrambot/models.py
@@ -1,12 +1,32 @@
 # -*- coding: utf-8 -*-
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+
+class Subscriber(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("User"))
+    name = models.CharField(max_length=250, verbose_name=_("Name"))
+    telegram_id = models.PositiveBigIntegerField(verbose_name=_("Telegram ID"))
+    is_bot = models.BooleanField(default=False, verbose_name=_("Is bot"))
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    updated = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.user.__str__()
+
+    class Meta:
+        verbose_name = _("Subscriber")
+        verbose_name_plural = _("Subscribers")
 
 
 class Request(models.Model):
-    subscriber = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('Subscriber'))
-    text = models.TextField(verbose_name=_('Text'))
+    subscriber = models.ForeignKey(
+        Subscriber, on_delete=models.CASCADE, verbose_name=_("Subscriber")
+    )
+    text = models.TextField(verbose_name=_("Text"))
     created = models.DateTimeField(auto_now_add=True, editable=False)
     edited = models.DateTimeField(auto_now=True, editable=False)
 
@@ -14,13 +34,15 @@ class Request(models.Model):
         return self.text
 
     class Meta:
-        verbose_name = _('Request')
-        verbose_name_plural = ('Requests')
+        verbose_name = _("Request")
+        verbose_name_plural = "Requests"
 
 
 class Response(models.Model):
-    subscriber = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('Subscriber'))
-    text = models.TextField(verbose_name=_('Text'))
+    subscriber = models.ForeignKey(
+        Subscriber, on_delete=models.CASCADE, verbose_name=_("Subscriber")
+    )
+    text = models.TextField(verbose_name=_("Text"))
     created = models.DateTimeField(auto_now_add=True, editable=False)
     edited = models.DateTimeField(auto_now=True, editable=False)
 
@@ -28,14 +50,20 @@ class Response(models.Model):
         return self.text
 
     class Meta:
-        verbose_name = _('Response')
-        verbose_name_plural = ('Responses')
+        verbose_name = _("Response")
+        verbose_name_plural = "Responses"
 
 
 class Dialog(models.Model):
-    subscriber = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('Subscriber'))
-    request = models.ForeignKey(Request, on_delete=models.CASCADE, verbose_name=_('Request'))
-    response = models.ForeignKey(Response, on_delete=models.CASCADE, verbose_name=_('Response'))
+    subscriber = models.ForeignKey(
+        Subscriber, on_delete=models.CASCADE, verbose_name=_("Subscriber")
+    )
+    request = models.ForeignKey(
+        Request, on_delete=models.CASCADE, verbose_name=_("Request")
+    )
+    response = models.ForeignKey(
+        Response, on_delete=models.CASCADE, verbose_name=_("Response")
+    )
     created = models.DateTimeField(auto_now_add=True, editable=False)
     edited = models.DateTimeField(auto_now=True, editable=False)
 
@@ -43,6 +71,6 @@ class Dialog(models.Model):
         return self.subscriber.__str__()
 
     class Meta:
-        verbose_name = _('Dialog')
-        verbose_name_plural = _('Dialogs')
-        ordering = ['-created']
+        verbose_name = _("Dialog")
+        verbose_name_plural = _("Dialogs")
+        ordering = ["-created"]
